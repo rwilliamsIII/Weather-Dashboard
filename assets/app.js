39bd88d08cbd75e5633f66304fe10c17e5e48daf
Robert Williams <robert@a3marketing.com>
2020-03-17 10:39:49 -0500

    created a function that will append search results to empty div

--- assets/app.js
+++ assets/app.js
@@ -4,3 +4,20 @@ console.log(currentDate);
 
 // global variables for ajax call and adding content to page
 var apiKey = "b03435d2b43059ea11fb5360dc72b23b";
+var submitCity = $("#submit-city");
+var cityHistory = $("#searchResults");
+var currentWeather = $("#currentWeather");
+var fiveDay = $("#fiveDay");
+var searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+
+
+// function to print search results to page using local storage
+function loadResults(){
+    for(var i = 0; i < searchHistory.length; i++) {
+        var resultsDiv = $("<div>");
+        resultsDiv.text(searchHistory[i]);
+        resultsDiv.attr("data-city", searchHistory[i]);
+        resultsDiv.addClass("searched-city");
+        resultsDiv.prependTo(cityHistory);
+    }
+}
