d5c667fdc8b78f88705d2f0ae25379806826e5e8
Robert Williams <robert@a3marketing.com>
2020-03-17 10:30:06 -0500

    created global variables for local storage and to place content on page

---
+++ assets/app.js
@@ -0,0 +1,6 @@
+// moment js to get current date and time
+var currentDate = moment().format("MMM Do YYYY, h:mm:ss a");
+console.log(currentDate);
+
+// global variables for ajax call and adding content to page
+var apiKey = "b03435d2b43059ea11fb5360dc72b23b";
